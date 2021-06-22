1c108e385ba33fad46e88b8151403c5e94811e5d
Gabriel Souza <gabrielcsg19@hotmail.com>
2021-06-22 20:27:11 -0300

    Finalizada aula 2

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -4,11 +4,10 @@ import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg';
 import '../styles/auth.scss';
 import { Button } from '../components/Button';
-import { useContext } from 'react';
-import { AuthContext } from '../App';
+import { useAuth } from '../hooks/useAuth';
 
 export function NewRoom() {
-  const { user } = useContext(AuthContext);
+  const { user } = useAuth();
 
   return (
     <div id="page-auth">
